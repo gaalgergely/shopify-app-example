65d5a97fffab5f0020f0bf512f7f2be605485a89
Gergely, Gaal <gaalgergely@users.noreply.github.com>
2021-08-11 15:50:17 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,35 @@
-import { Heading, Page } from "@shopify/polaris";
 
-const Index = () => (
-  <Page 
+import { ResourcePicker } from "@shopify/app-bridge-react";
+import { Page } from "@shopify/polaris";
+import React, { useState } from 'react'
+
+export function index() {
+
+  const [isOpen, setIsOpen] = useState(false)
+  const [products, setProducts] = useState([])
+
+  function handleProductSelection(payload) {
+    setIsOpen(false);
+    setProducts(payload.selection);
+  }
+
+  return (
+    <Page 
     title="Product Selector"
     primaryAction={{
       content: "Select product",
-      onAction: () => console.log("I have been clicked")
+      onAction: () => setIsOpen(true)
     }}  
   >
+    <ResourcePicker 
+      resourceType="Product"
+      open={isOpen}
+      onCancel={() => setIsOpen(false)}
+      onSelection={handleProductSelection}
+    />
+    {products.map(product => <div>{product.title}</div>)}
   </Page>
-);
+  )
+}
 
-export default Index;
+export default index;
